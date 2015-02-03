82370b688cda2220742039a41442adf711a17d27
tsuhovenko <tsuhovenko@cogniance.com>
2015-02-03 16:54:02 +0200

    table with angular

--- src/main/webapp/assets/js/hello.js
+++ src/main/webapp/assets/js/hello.js
@@ -1,7 +1,12 @@
 (function() {
     var app = angular.module('blog', []);
 
-    app.controller('HelloController', function() {
-        this.greeting = "test";
-    });
+    app.controller('HelloController', ['$http', function($http) {
+        var hello = this;
+        hello.users = {};
+        $http.get('http://localhost:8010/rest/users').success(function(data) {
+            hello.users = data._embedded.userList;
+            console.log(hello.users);
+        });
+    }]);
 })();
